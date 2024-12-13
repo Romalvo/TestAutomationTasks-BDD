10ab3a21134cb025b8c438bb723f4fe4d71dc325
Romalvo <roman.voronkov.06@gmail.com>
2024-12-13 17:53:56 +0200

    Created language switsching steps

---
+++ cypress/support/step_definitions/languageSwitchingSteps.js
@@ -0,0 +1,54 @@
+import HomePage from "../pageObjects/HomePage";
+import {Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+import { setViewport } from "../viewportHelper";
+
+const homePage = new HomePage();
+
+Given('I am on the Boost Casino home page using on {string} view', (device) => {
+    setViewport(device);
+    cy.visit('https://www.boostcasino.com/');
+});
+
+Then('I should see the language dropdown in the header', () => {
+    homePage.verifyLanguageDropdownVisible();
+});
+
+When('I click on the language dropdown', () => {
+    homePage.clickLanguageDropdown();
+});
+
+Then('the language menu should be visible', () => {
+    homePage.verifyLanguageMenuVisible();
+});
+
+When('I select "FI" from the dropdown', () => {
+    homePage.selectLanguage("FI");
+});
+
+Then('the page should switch to Finnish', () => {
+    homePage.verifyCorrectLinkLoaded('FI');
+});
+
+When('I click on the language dropdown to choose Estonian language', () => {
+    homePage.clickLanguageDropdown();
+});
+
+When('I select "EE" from the dropdown', () => {
+    homePage.selectLanguage("EE");
+});
+
+Then('the page should switch to Estonian', () => {
+    homePage.verifyCorrectLinkLoaded('EE');
+});
+
+When('I click on the language dropdown to choose Russian language', () => {
+    homePage.clickLanguageDropdown();
+});
+
+When('I select "RU" from the dropdown', () => {
+    homePage.selectLanguage("RU");
+});
+
+Then('the page should switch to Russian', () =>{
+    homePage.verifyCorrectLinkLoaded('RU');
+});
