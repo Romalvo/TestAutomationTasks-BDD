3966fb45b12c8eeca97451f1593a0a5dbe42f002
Romalvo <roman.voronkov.06@gmail.com>
2024-12-13 12:21:03 +0200

    Created viewports

---
+++ cypress/config/viewports.js
@@ -0,0 +1,15 @@
+const viewports = {
+    desktop: {
+        width: 1280,
+        height: 720,
+    },
+    tablet: {
+        width: 768,
+        height: 1024,
+    },
+    mobile: {
+        width: 375,
+        height: 667,
+    },
+}
+export default viewports;
