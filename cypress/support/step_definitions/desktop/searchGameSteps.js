d617ce6d35970813bd47beaeb1a3664bf5bd7f79
Romalvo <roman.voronkov.06@gmail.com>
2024-12-14 21:24:10 +0200

    Updated search game steps

---
+++ cypress/support/step_definitions/desktop/searchGameSteps.js
@@ -0,0 +1,35 @@
+import HomePage from "../../pageObjects/HomePage.js";
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+import { setViewport } from "../../viewportHelper.js";
+
+const homePage = new HomePage;
+
+Given('I am on the Boost Casino home page using {string} view', (device) => {
+    setViewport(device);
+    cy.visit('/');
+});
+
+Then('I should see the search input field', () => {
+    homePage.verifySearchInputVisible();
+});
+
+When('I search for "Book of Dead"', (gameName) => {
+    homePage.searchForGame(gameName);
+});
+
+Then('I should see the "Book of Dead" game listed in the results', (gameName) => {
+    homePage.verifyGameSearchResults(gameName);
+});
+
+When('I click on the "Play" button', (buttonText) => {
+    homePage.clickPlayButton(buttonText);
+});
+
+Then('I should be redirected to the games play page', (gameName) => {
+    homePage.verifyGamePlayPage(gameName);
+});
+
+Then('the game should load successfully', (gameName) => {
+    homePage.verifyGameLoading;
+});
+
